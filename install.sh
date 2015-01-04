d402c8ce9d419df2dde229a1e65f88b92ad851e2
ekultails <ekultails@gmail.com>
2015-01-04 15:02:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ sed -i s/Subsystem/\#Subsystem/g /etc/ssh/sshd_config
 echo -e "Subsystem\tsftp\tinternal-sftp\nAllowGroups sftpusers\nMatch Group sftpusers\n\tChrootDirectory %h\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no" >> /etc/ssh/sshd_config
 groupadd sftpusers
 
-service sshd reload
+service sshd restart
 
 
