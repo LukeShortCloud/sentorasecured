1619dcb1b25614b9a5a2aa4d4c398bc4f0cb88ce
ekultails <ekultails@gmail.com>
2015-01-04 17:22:11 -0500

    Update ss_users.sh

--- ss_users.sh
+++ ss_users.sh
@@ -3,7 +3,9 @@ for i in `mysql -e 'use sentora_core; select ac_user_vc from x_accounts' | grep 
 do useridandpasswd=$(mysql -e 'use sentora_proftpd; select userid,passwd from ftpuser;'| grep $i);
 userid=$(echo $useridandpasswd | awk {'print $1'}); pass=$(echo $useridandpasswd | awk {'print $2'});
 echo "Resetting SFTP password for $userid"
-useradd $userid
-usermod -d /var/sentora/hostdata/$userid -G sftpusers $userid
+useradd $userid; groupadd $userid
+
+#Set home directory for their hostdata folder, change their default group to themselves, and add them to the sftpusers for jailed SFTP access
+usermod -d /var/sentora/hostdata/$userid -g $userid -a -G sftpusers $userid
 echo "$userid:$pass" | chpasswd -m
 done
