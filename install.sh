b8dbbce3939aa108c544783e947c74e7d1f03d6b
ekultails <ekultails@gmail.com>
2015-01-04 15:26:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp -a /etc/ssh/sshd_config /etc/ssh/sshd_config.$(date +%m-%d-%Y_%H_%M_%S)
 
 #Get the SFTP jailed shell environment ready.
 sed -i s/Subsystem/\#Subsystem/g /etc/ssh/sshd_config
-echo -e "Subsystem\tsftp\tinternal-sftp\nAllowGroups sftpusers\nMatch Group sftpusers\n\tChrootDirectory %h\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no" >> /etc/ssh/sshd_config
+echo -e "Subsystem\tsftp\tinternal-sftp\nAllowGroups root sftpusers\nMatch Group sftpusers\n\tChrootDirectory %h\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no" >> /etc/ssh/sshd_config
 groupadd sftpusers
 
 service sshd restart
