1fe90a4552f20a4d4ba51500c1edf4a599bdc0e9
ekultails <ekultails@gmail.com>
2015-01-04 15:04:00 -0500

    Update ss_permissions.sh

--- ss_permissions.sh
+++ ss_permissions.sh
@@ -1,5 +1,5 @@
 #STUB
 #/var/sentora = 755 root.apache
 #/var/sentora/hostdata = 755 root.apache
-#/var/sentora/hostdata/USER = 750 apache.sftpusers
+#/var/sentora/hostdata/USER = 750 root.sftpusers
 #/var/sentora/hostdata/USER/DOMAIN_COM = 700 USER.sftpusers
