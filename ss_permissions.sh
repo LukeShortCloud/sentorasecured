5408001875084e7128241dc481d99fa7eff9af5c
ekultails <ekultails@gmail.com>
2015-01-04 15:16:55 -0500

    Update ss_permissions.sh

--- ss_permissions.sh
+++ ss_permissions.sh
@@ -1,4 +1,5 @@
 #STUB
+#For jailed SFTP access to work, folders leading up to their home directory /var/sentora/hostdata/USER/ MUST be owned by the user root and ONLY be writable by the user root.
 #/var/sentora = 755 root.apache
 #/var/sentora/hostdata = 755 root.apache
 #/var/sentora/hostdata/USER = 750 root.sftpusers
