0f3e8a61f0b907ff2a31e66e6ebb0c957712e395
ekultails <ekultails@gmail.com>
2015-01-04 15:35:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-#Sentora Secured installer
+#!/bin/sh
+###SENTORA SECURED - Installer
 
+#Create Linux users
+sh ./ss_users.sh
 
 #Backup the original sshd configuraiton
 cp -a /etc/ssh/sshd_config /etc/ssh/sshd_config.$(date +%m-%d-%Y_%H_%M_%S)
+echo "Your original sshd configuration has been saved to $(\ls -t /etc/ssh/ | head -n 2 | grep -P [0-9])"
 
 #Get the SFTP jailed shell environment ready.
 sed -i s/Subsystem/\#Subsystem/g /etc/ssh/sshd_config
