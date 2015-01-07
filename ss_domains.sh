9d31334d98a837538cd261673d4e3c5246595a6f
ekultails <ekultails@gmail.com>
2015-01-06 20:14:04 -0500

    Update ss_domains.sh

--- ss_domains.sh
+++ ss_domains.sh
@@ -2,7 +2,7 @@
 #This script will create a list of all of the domains that a user owns to /etc/sentora/trueuserdomains
 
 #Empty/create the build files
-> /tmp/users.id; > /tmp/users.name; > /etc/sentora/trueuserdomains;
+> /tmp/users.id; > /tmp/users.name; > /var/sentora/secured/trueuserdomains.txt;
 
 #Find all the user identification numbers
 for i in `mysql -e 'use sentora_core; select ac_id_pk from x_accounts' |grep -v ac_id_pk`; 
@@ -19,10 +19,10 @@ done;
 for x in `cat /tmp/users.name`;  
 	do id=$(echo $x | cut -d: -f1);
 	for z in `mysql -e 'use sentora_core; select vh_name_vc from x_vhosts where vh_acc_fk="'"$id"'";' | grep -v vh_name_vc`
-		do echo "${x}${z}" >> /etc/sentora/trueuserdomains;
-	done; 
+		do echo "${x}${z}" >> /var/sentora/secured/trueuserdomains.txt;
+	done;
 done;
 
 #Cleanup
-rm -rf /tmp/users.*; chown root: /etc/sentora/trueuserdomains; chmod 660 /etc/sentora/trueuserdomains
+rm -rf /tmp/users.*; chown root: /var/sentora/secured/trueuserdomains.txt; chmod 660 /var/sentora/secured/trueuserdomains.txt
 
