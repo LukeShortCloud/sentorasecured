c954728374c472d7e256c451dc87c2190acb497b
ekultails <ekultails@gmail.com>
2015-01-06 22:43:17 -0500

    Update ss_permissions.sh

--- ss_permissions.sh
+++ ss_permissions.sh
@@ -14,8 +14,11 @@ for i in `cat /tmp/perms.build`; do echo "$(stat ${i} | grep -Po "(-|d|l)(-|r|w|
 #For jailed SFTP access to work, folders leading up to their home directory /var/sentora/hostdata/USER/ MUST be owned by the user root and ONLY be writable by the user root.
 chmod 755 /var/sentora; chown root.apache /var/sentora;
 chmod 755 /var/sentora/hostdata; chown root.apache /var/sentora/hostdata;
-#chmod 755 /var/sentora/hostdata/$userid; chown root.sftpusers /var/sentora/hostdata/$userid
-#/var/sentora/hostdata/USER/DOMAIN_COM = 700 USER.sftpusers
+
+#Correct user permissions for RUID2 so their processes run as the actual Linux user
+for userid in `cat /var/sentora/secured/trueuserdomains.txt | cut -d: -f2 | uniq`;
+  do find /var/sentora/hostdata/${userid}/ -name "*" -exec chown ${userid}.${userid} {} \;
+done
 
 #General Sentora permissions fix
 find /etc/sentora -type f -exec chmod 644 {} \;
