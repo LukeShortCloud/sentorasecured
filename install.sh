0b89db139f3709642b340d19c68e1c3370426574
ekultails <ekultails@gmail.com>
2015-01-06 21:02:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,27 +2,36 @@
 ###SENTORA SECURED - Installer
 
 #Set up Sentora Secured's installation directory
-mkdir /var/sentora/secured/ /var/sentora/secured/uninstall/
+mkdir /var/sentora/secured/ /var/sentora/secured/uninstall/ /var/sentora/secured/old
 chown root.root /var/sentora; chmod 770 /var/sentora/
 
 #Sort out domains and their respective users
 sh ./ss_domains.sh
 
-#Create Linux users
-sh ./ss_users.sh
-
 #Fix insecure permissions
 sh ./ss_permissions.sh
 
+#Create Linux users
+sh ./ss_users.sh
+
 #Backup the original sshd configuraiton
-cp -a /etc/ssh/sshd_config /etc/ssh/sshd_config.$(date +%m-%d-%Y_%H_%M_%S)
-echo "Your original sshd configuration has been saved to $(\ls -t /etc/ssh/ | head -n 2 | grep -P [0-9])"
+cp -a /etc/ssh/sshd_config /var/sentora/secured/uninstall/sshd_config
+echo "Your original sshd configuration has been saved to /var/sentora/secured/uninstall/sshd_config"
 
-#Get the SFTP jailed shell environment ready.
+##Get the SFTP jailed shell environment ready.
+
+#Disable/comment-out possible conflicting entries in the SSH configuration file
 sed -i s/Subsystem/\#Subsystem/g /etc/ssh/sshd_config
-echo -e "Subsystem\tsftp\tinternal-sftp\nAllowGroups root sftpusers\nMatch Group sftpusers\n\tChrootDirectory %h\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no" >> /etc/ssh/sshd_config
-groupadd sftpusers
+sed -i s/AllowUsers/\#AllowUsers/g /etc/ssh/sshd_config
+sed -i s/AllowGroups/\#AllowGroups/g /etc/ssh/sshd_config
+sed -i s/Match/\#Match/g /etc/ssh/sshd_config
+sed -i s/ChrootDirectory/\#ChrootDirectory/g /etc/ssh/sshd_config
+sed -i s/ForceCommand/\#ForceCommand/g /etc/ssh/sshd_config
+sed -i s/X11Forwarding/\#X11Forwarding/g /etc/ssh/sshd_config
+sed -i s/AllowTcpForwarding/\#AllowTcpForwarding/g /etc/ssh/sshd_config
 
+#Enable jailed SFTP 
+echo -e "Subsystem\tsftp\tinternal-sftp\nAllowGroups root sftpusers\nMatch Group sftpusers\n\tChrootDirectory %h\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no"
 service sshd restart
 
 
