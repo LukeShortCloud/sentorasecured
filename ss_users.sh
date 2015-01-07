4c6a908083023331a6415b0c0cb8ef3fb076a490
ekultails <ekultails@gmail.com>
2015-01-06 22:38:18 -0500

    Update ss_users.sh

--- ss_users.sh
+++ ss_users.sh
@@ -1,14 +1,17 @@
-#This will be set up as a cron that will continually check for password changes to the FTP user and then change it for the respective Linux user.
+#!/bin/sh
+###SENTORA SECURED - Users - Linux user creation
+
+#This should be set up as a cron that will continually check for password changes to the FTP user and then change it for the respective Linux user.
 for i in `mysql -e 'use sentora_core; select ac_user_vc from x_accounts' | grep -P "[a-z]*[A-Z]*[0-9]*" | grep -v ac_user_vc`;
 do useridandpasswd=$(mysql -e 'use sentora_proftpd; select userid,passwd from ftpuser;'| grep $i);
 userid=$(echo $useridandpasswd | awk {'print $1'}); pass=$(echo $useridandpasswd | awk {'print $2'});
 echo "Resetting SFTP password for $userid"
-useradd $userid; groupadd $userid
+useradd $userid 2&>1 /dev/null; groupadd $userid 2&>1 /dev/null;
 
 #Set home directory for their hostdata folder, change their default group to themselves, and add them to the sftpusers for jailed SFTP access
-usermod -d /var/sentora/hostdata/$userid -g $userid -a -G sftpusers $userid
+usermod -d /var/sentora/hostdata/$userid -g $userid -a -G sftpusers $userid;
 #MD5 password encryption is used
-echo "$userid:$pass" | chpasswd -m
+echo "$userid:$pass" | chpasswd -m;
 done
 
 
