9f01b903cda6156d58ba0cda2a9a786ddc5b6184
ekultails <ekultails@gmail.com>
2015-01-12 00:09:17 -0500

    Update ss_domains.sh

--- ss_domains.sh
+++ ss_domains.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#This script will create a list of all of the domains that a user owns to /etc/sentora/trueuserdomains
+#This script will create a list of all of the domains that a user owns to /var/sentora/secured/trueuserdomains.txt
 
 #Empty/create the build files as well as back up the origianl trueuserdomains.txt
 if [[ -f /var/sentora/secured/trueuserdomains.txt ]]
