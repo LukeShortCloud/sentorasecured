a0ceebd5b095648e479db01c0ddb317c64155555
ekultails <ekultails@gmail.com>
2015-01-14 13:00:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###SENTORA SECURED - Installer
-##v0.1-3
+##v0.2
 
 #Set up Sentora Secured's installation directory
 mkdir /var/sentora/secured/ /var/sentora/secured/uninstall/ /var/sentora/secured/old 2&>1 /dev/null
