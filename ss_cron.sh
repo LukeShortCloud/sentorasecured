eeebfcba7c3a1a3f2446ae6d662d2e635e125a8c
ekultails <ekultails@gmail.com>
2015-01-15 01:36:57 -0500

    Update ss_cron.sh

--- ss_cron.sh
+++ ss_cron.sh
@@ -2,6 +2,9 @@
 ### SENTORA SECURED - CRON - ss_cron.sh
 ##This cron job will continually add in any new Linux/SFTP users, reset their FTP password to the current one, and correct permissions.
 
+#Refresh the domain-user ownership file
+sh /var/sentora/secured/ss_domains.sh
+
 #Setup new jailed SFTP/Linux users and reset any new passwords
 sh /var/sentora/secured/ss_users.sh
 
